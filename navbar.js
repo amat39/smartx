3bb46a3216838ec5e37055e1536f97ff951bab55
Mohammad Yasin <amatyasin1@gmail.com>
2017-11-26 05:33:17 +0800

    ses

---
+++ navbar.js
@@ -0,0 +1,21 @@
+var sessi = 0;
+function checksession(sessi){
+	if (sessi>=0) {
+		<a style="float: left;" href="index.php">Home</a>
+		  <a style="float: left;" href="#">Products</a>
+      
+      <a href="login.php">Login</a>
+      <a href="reigstration.php">Registration</a>
+	<a href="#">Contacts</a>
+	
+	}
+	
+
+	else {
+
+		<a style="float: left;" href="index.php">Home</a>
+		  <a style="float: left;" href="#">Products</a>
+      
+		  <a href="#">Contacts</a>
+	}
+}
---
+++ slideshow.php
@@ -0,0 +1,33 @@
+ <div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="pic/Hachikuji.Mayoi.full.1589121.jpg" style="width:100%">
+  <div class="text">perkenthodan</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="pic/hachikuji-mayoi_00359357.jpg" style="width:100%">
+  <div class="text">memekkkkk</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="pic/mayoi-hachikuji-bakemonogatari-23156-1680x1050 (1).jpg" style="width:100%">
+  <div class="text">peler</div>
+</div>
+
+
+<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span>
+  <span class="dot" onclick="currentSlide(4)"></span>  
+</div>
